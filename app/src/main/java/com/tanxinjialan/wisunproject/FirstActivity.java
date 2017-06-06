f86b7c0f1570d73482148a8dfefbae7d3de959a2
Tan Xin Ji Alan <tan.xin.ji.alan@gmail.com>
2017-06-06 11:28:19 +0800

    Add Google Maps to FirstActivity.java

--- app/src/main/java/com/tanxinjialan/wisunproject/FirstActivity.java
+++ app/src/main/java/com/tanxinjialan/wisunproject/FirstActivity.java
@@ -3,11 +3,36 @@ package com.tanxinjialan.wisunproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FirstActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FirstActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    Contact arrayContact[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_first);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng singapore = new LatLng(1.37, 103.81);
+        googleMap.addMarker(new MarkerOptions().position(singapore)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
     }
 }

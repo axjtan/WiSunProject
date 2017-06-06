cb0ecb29ca7b3edcb52151eb4451e759b36b5985
Tan Xin Ji Alan <tan.xin.ji.alan@gmail.com>
2017-06-06 14:50:37 +0800

    A styled map using JSON styles from a string resource (style_strings.xml).

--- app/src/main/java/com/tanxinjialan/wisunproject/FirstActivity.java
+++ app/src/main/java/com/tanxinjialan/wisunproject/FirstActivity.java
@@ -1,14 +1,16 @@
 package com.tanxinjialan.wisunproject;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MapStyleOptions;
+        import com.google.android.gms.maps.model.MarkerOptions;
 
 public class FirstActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -20,19 +22,27 @@ public class FirstActivity extends AppCompatActivity implements OnMapReadyCallba
 
         // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_first);
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready for use.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
+        // Customise the styling of the base map using a JSON object defined in a string resource file. First create a MapStyleOptions object
+        // from the JSON styles string, then pass this to the setMapStyle method of the GoogleMap object.
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources().getString(R.string.style_json)));
+        if (!success) {
+            Log.e("Error", "Style parsing failed.");
+        }
+        // Add a marker in Singapore and move the map's camera to the same location.
         LatLng singapore = new LatLng(1.37, 103.81);
-        googleMap.addMarker(new MarkerOptions().position(singapore)
-                .title("Marker in Sydney"));
+        googleMap.addMarker(new MarkerOptions().position(singapore).title("Marker in Singapore"));
+        // Position the map's camera in centre of Singapore.
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
     }
 }
